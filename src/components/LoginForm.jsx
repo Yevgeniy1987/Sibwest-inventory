df5875e9cd9be836e520acabfef58447407c64ee
Eugene <evgeniy.lyashko.87@gmail.com>
2023-09-24 22:41:40 -0500

    fix mistakes

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,15 +1,10 @@
 import classNames from "classnames";
 import { useAuthState } from "../context/AuthContext";
 import { api } from "../service/api";
-import { useState } from "react";
-
-
 
 export const LoginForm = () => {
   const [authState] = useAuthState();
   const { logIn } = authState;
-
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,33 +25,31 @@ export const LoginForm = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col gap-2 items-center justify-center"
-    >
-      <input
-        type="text"
-        name="email"
-        placeholder="email"
-        value={"olivier@mail.com"}
-        className="border p-1 border-solid rounded border-black w-full"
-      />
-      <input
-        type="text"
-        name="password"
-        placeholder="password"
-        value={"bestPassw0rd"}
-        className="border p-1 border-solid rounded border-black w-full"
-      />
-      <button
-        type="submit"
-        className={classNames(
-          "px-8 py-4 mt-3 bg-green-600 border border-solid border-white rounded text-white hover:text-black hover:bg-white",
-          isLoading && "bg-gray-400"
-        )}
-      >
-        Login
-      </button>
+    <form onSubmit={handleSubmit} className="w-full">
+      <div className="flex flex-col gap-2 items-center justify-center">
+        <input
+          type="text"
+          name="email"
+          placeholder="email"
+          value={"olivier@mail.com"}
+          className="border p-1 border-solid rounded border-black"
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          value={"bestPassw0rd"}
+          className="border p-1 border-solid rounded border-black "
+        />
+        <button
+          type="submit"
+          className={classNames(
+            "px-8 py-4 mt-3 bg-green-600 border border-solid border-white rounded text-white hover:text-black hover:bg-white"
+          )}
+        >
+          Login
+        </button>
+      </div>
     </form>
   );
 };
